7968708f55e846ab1d0baea017aa190ccaee9179
Jens Skott <jens.skott@checkatrade.com>
2024-06-17 06:30:33 +0100

    updated the template

--- terraform/modules/cloud_run_factory/main.tf
+++ terraform/modules/cloud_run_factory/main.tf
@@ -1,3 +1,12 @@
+locals {
+  service_apis = [
+    "artifactregistry.googleapis.com",
+    "run.googleapis.com",
+    "cloudbuild.googleapis.com",
+    "sercretmanager.googleapis.com",
+  ]
+}
+
 resource "google_service_account" "this" {
   project      = var.project_id
   account_id   = "run-${var.service_name}"
