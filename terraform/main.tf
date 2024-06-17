2b69a6d45934363341ab9a70283521344d77c868
Jens Skott <jens.skott@checkatrade.com>
2024-06-17 06:37:44 +0100

    updated to store in state

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,13 @@
 terraform {
   required_version = ">= 1.8.2"
+  
+  backend "gcs" {
+    bucket = "${var.project_id}-terraform-state"
+    prefix = "demo-resource-scaffolding"
+  }
+}
+
+variable "project_id" {
+  type        = string
+  description = "The project ID to deploy the resources"
 }
